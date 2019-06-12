5fa8b08bff26c597b8704d34bef6d9ce947ee1a0
Daniel <danieldanila26@gmail.com>
2019-06-12 20:07:29 +0300

    hey

--- curs5/src/NumberSum.java
+++ curs5/src/NumberSum.java
@@ -1,9 +1,13 @@
 public class NumberSum {
 
+    public static void main(String[] args) {
+        System.out.println(calculate( 20));
+    }
+
     public static int calculate(int a){
         int b=0;
         for (int i=0; i<=a; i++){
-            if (a%2==0){
+            if (i%2==0){
                 b=b+i;
             }
 
@@ -12,7 +16,5 @@ public class NumberSum {
 
     }
 
-    public static void main(String[] args) {
-        System.out.println(calculate(10));
-    }
+
 }
